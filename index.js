4a93afbcec893ec283a2b039ed585a719a01145c
Leonardo Santos <leosantos.vinte@gmail.com>
2023-02-24 20:41:13 -0300

    Section 5 class 25 Using variables with HTML

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const app = express();
 //using EJS as view engine
 app.set('view engine', 'ejs');
 
-app.get("/", (request, response) => {
-   response.render("main/profile");
+app.get("/:name/:language", (request, response) => {
+   var name = request.params.name;
+   var language = request.params.language;
+   response.render("index",{
+      name: name,
+      language: language,
+      company: "Guia do programador",
+      subscribes: 8000
+   });
 });
 
 app.listen(4000,() => {console.log("App is working now...");});
