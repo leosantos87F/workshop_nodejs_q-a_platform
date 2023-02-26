7014e9bfd10fa38f59d653113d63b89e32c0b1bf
Leonardo Santos <leosantos.vinte@gmail.com>
2023-02-25 21:49:09 -0300

    Section 5 class 29 - Bootstrap Installation and script JQuery and Pooper installation

--- index.js
+++ index.js
@@ -5,31 +5,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
-
-//variables and get method settings
-app.get("/:name/:language", (request, response) => {
-   var name = request.params.name;
-   var language = request.params.language;
-   var showMsg = true;
-
-   var products = [
-      {name: "Doritos", price: 3.14},
-      {name: "Coca-Cola", price: 5.00},
-      {name: "Milk", price: 1.45},
-      {name: "Meat", price: 5.00},
-      {name: "Red Bull", price: 6.00},
-      {name: "Chocolate Powder", price: 4.00},
-  ]
-
-   //rendering variables values
-   response.render("index",{
-      name: name,
-      language: language,
-      company: "Guia do programador",
-      subscribes: 8000,
-      msg: showMsg,
-      products: products
-   });
+app.get("/", (request, response) => {
+  response.render("index");
 });
 
 app.listen(4000,() => {console.log("App is working now...");});
