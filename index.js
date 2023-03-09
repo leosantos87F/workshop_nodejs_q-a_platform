b01832d4f25e28a560003f021cc7f067186e2c72
Leonardo Santos <leosantos.vinte@gmail.com>
2023-03-09 18:54:57 -0300

    Section 5 classes 35-39 Adding Node npm install Body Parser save and getting info from form

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //using EJS as view engine
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
+//body parser api configuration
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.get("/", (request, response) => {
   response.render("index");
 });
 
-app.get("/ask", (request, response) => {
+app.get("/question", (request, response) => {
   response.render("question");
 });
+
+app.post("/savequestion",(request, response) => {
+  var title = request.body.title;
+  var description = request.body.description;
+  response.send("Form has been received! title " + title + " " + " description " + description);
+});
+
 
 app.listen(4000,() => {console.log("App is working now...");});
